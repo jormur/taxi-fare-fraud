2f52363add779400eaa65ec6e7e39209df163967
Jordan Murillo <jordanmurillo21@gmail.com>
2024-01-09 02:43:01 +0800

    Corrected output

--- summary_tbls.R
+++ summary_tbls.R
@@ -14,23 +14,23 @@ library(kableExtra)
 driver_summary_table <- taxi %>%
   group_by(PULocationID) %>%
   summarize(
-    num_providers = length(unique(VendorID)),
     num_trips = n(),
     working_hours = sum(ceiling(ifelse(dropoff_hour < pickup_hour, 24 + dropoff_hour - pickup_hour, trip_duration / 60))),  # Round up to the nearest whole number
     occupied_hours = sum(ifelse(passenger_count > 0, trip_duration / 60, 0)),  # Convert trip_duration to hours
-    occupancy = occupied_hours / working_hours,
-    num_trips_LGA = sum(PULocationID == 138),  # Replace LGA_Location_ID with the actual Location ID for LGA
-    num_trips_JFK = sum(PULocationID == 132)   # Replace JFK_Location_ID with the actual Location ID for JFK
+    occupancy = occupied_hours / working_hours
   )
 
-# Use knitr to format the table
-kable(driver_summary_table, caption = "Driver Characteristics by Departure Location and Trip Type")
+#Rename PUlocationID to Airport
+driver_summary_table <- driver_summary_table %>%
+  rename(Airport = PULocationID)
 
-#Add a column for the stats of all trips
+#Change Airport values to JK and LGA
+driver_summary_table$Airport[driver_summary_table$Airport == 132] <- "JFK"
+driver_summary_table$Airport[driver_summary_table$Airport == 138] <- "LGA"
 
 driver_summary_table %>%
-  kbl(caption = "Driver Characteristics") %>%
-  kable_classic(full_width = F, html_font = "Cambria")
+  kbl(caption = "Table II: Driver Characteristics") %>%
+  kable_classic(full_width = F, html_font = "Georgia")
 
 # TABLE III #
 #Create table summary statistics including the number of trips, the average trip distance, duration and fare
@@ -47,10 +47,17 @@ table_summary <- taxi %>%
     avg_fare = mean(fare_amount)
   )
 
-# Use knitr to format the table
-kable(table_summary, caption = "Summary Statistics by Departure Location and Trip Type")
+#Rename PUlocationID to Airport
+table_summary <- table_summary %>%
+  rename(Airport = PULocationID)
 
+#Change Airport values to JK and LGA
+table_summary$Airport[table_summary$Airport == 132] <- "JFK"
+table_summary$Airport[table_summary$Airport == 138] <- "LGA"
 
+table_summary %>%
+  kbl(caption = "Table III: Summary Statistics by Departure Location and Trip Type") %>%
+  kable_classic(full_width = F, html_font = "Georgia")
 
 
 
